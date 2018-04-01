8aa23f986becdc81b2deb640406e5f1cc9346609
juju4 <juju4@users.noreply.github.com>
2018-04-01 08:09:17 -0400

    tls12: fix syntax

--- controls/tls12.rb
+++ controls/tls12.rb
@@ -10,12 +10,12 @@ if tls12_check
     desc 'Ensure that TLS 1.2 is enabled in registry - reboot required'
     ref url: 'https://technet.microsoft.com/en-us/library/dd560644(v=ws.10).aspx'
     ref url: 'https://support.microsoft.com/en-us/help/245030/how-to-restrict-the-use-of-certain-cryptographic-algorithms-and-protoc'
-    describe registry_key('HKEY_LOCAL_MACHINE:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client') do
+    describe registry_key('HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client') do
       it { should exist }
       its('DisabledByDefault') { should eq 0 }
       its('Enabled') { should eq 1 }
     end
-    describe registry_key('HKEY_LOCAL_MACHINE:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server') do
+    describe registry_key('HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server') do
       it { should exist }
       its('DisabledByDefault') { should eq 0 }
       its('Enabled') { should eq 1 }
@@ -26,22 +26,22 @@ if tls12_check
     impact 0.7
     title 'SSLv3 check'
     desc 'Ensure that SSLv3 and older are disabled in registry'
-    describe registry_key('HKEY_LOCAL_MACHINE:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client') do
+    describe registry_key('HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client') do
       it { should exist }
       its('DisabledByDefault') { should eq 0 }
       its('Enabled') { should eq 1 }
     end
-    describe registry_key('HKEY_LOCAL_MACHINE:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server') do
+    describe registry_key('HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server') do
       it { should exist }
       its('DisabledByDefault') { should eq 0 }
       its('Enabled') { should eq 1 }
     end
-    describe registry_key('HKEY_LOCAL_MACHINE:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client') do
+    describe registry_key('HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client') do
       it { should exist }
       its('DisabledByDefault') { should eq 0 }
       its('Enabled') { should eq 0 }
     end
-    describe registry_key('HKEY_LOCAL_MACHINE:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server') do
+    describe registry_key('HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server') do
       it { should exist }
       its('DisabledByDefault') { should eq 0 }
       its('Enabled') { should eq 0 }
